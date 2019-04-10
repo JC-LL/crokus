0e0a8fd7c4c572c0abccb0ea33bb86af23afdaaa
lelannje <jean-christophe.le_lann@ensta-bretagne.fr>
2019-04-10 18:14:06 +0200

    suppressed [a-s]*.c files

--- tests/new_arbre.c
+++
@@ -1,8 +0,0 @@
-
-// Fonction qui creer un arbre bianire
-void dummy(){
-}
-
-arbre * new_arbre(int a, int b, int c){
-}
-//
--- tests/new_arbre_pp.c
+++
@@ -1,6 +0,0 @@
-
-void dummy() {
-}
-
-arbre * new_arbre(int a,int b,int c) {
-}
